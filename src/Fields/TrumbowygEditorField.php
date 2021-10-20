e244ddf842abb85492a0f83881ba2c18ffd9fb85
James (DPC) <james.ellis@dpc.nsw.gov.au>
2021-10-20 18:23:37 +1100

    Update jQuery to 3.6.0

--- src/Fields/TrumbowygEditorField.php
+++ src/Fields/TrumbowygEditorField.php
@@ -80,9 +80,9 @@ class TrumbowygEditorField extends TextareaField {
 
         if($this->config()->get('include_own_jquery')) {
             Requirements::javascript(
-                "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js",
+                "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js",
                 [
-                    "integrity" => "sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==",
+                    "integrity" => "sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==",
                     "crossorigin" => "anonymous"
                 ]
             );
